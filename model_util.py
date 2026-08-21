373b0d25dcba1286c5c6e294078620ef51e201ae
charlien74 <charlieneuhaus@gmail.com>
2026-08-21 12:06:46 -0400

    Make duration change backward-compatible

--- model_util.py
+++ model_util.py
@@ -3,6 +3,14 @@ from brian2 import *
 RANDOM_SEED = 2
 # Simulation settings
 defaultclock.dt = 0.1 * ms
+duration_ms = 2000.0
+duration = duration_ms * ms
+
+def set_simulation_duration_ms(value_ms):
+	"""Update the shared simulation duration used by the scripts."""
+	global duration_ms, duration
+	duration_ms = float(value_ms)
+	duration = duration_ms * ms
 
 N_exc_c = 320
 N_inh = 700 
